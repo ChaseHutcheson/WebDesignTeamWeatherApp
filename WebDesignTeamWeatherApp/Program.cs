7fa27254cc0ab1348f34f0445f77165fe81a73d7
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2023-09-19 13:03:57 -0400

    Finished API call

--- WebDesignTeamWeatherApp/Program.cs
+++ WebDesignTeamWeatherApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Text.Json;
+using Newtonsoft.Json.Linq;
 
 namespace WebDesignTeamWeatherApp
 {
@@ -9,34 +9,27 @@ namespace WebDesignTeamWeatherApp
     {
         static async Task GetWeatherData()
         {
-
             using (HttpClient client = new HttpClient())
             {
-                // Specify the correct BaseAddress with protocol and domain
-                client.BaseAddress = new Uri("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=apparent_temperature,precipitation_probability&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto&forecast_days=14");
+                client.BaseAddress = new Uri("http://api.weatherapi.com/v1/forecast.json?key=37d68cfe17b446eea21145120231909&q=London&days=10");
 
                 try
                 {
-                    // Build the query string with parameters
-    
-
                     HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
 
                     if (response.IsSuccessStatusCode)
                     {
                         string response_body = await response.Content.ReadAsStringAsync();
 
-                        var options = new JsonSerializerOptions
+                        JObject jsonResponse = JObject.Parse(response_body);
+
+                        JToken weather = jsonResponse["forecast"]["forecastday"];
+
+                        for (int i = 0; i < 10; i++)
                         {
-                            WriteIndented = true, // Format the JSON with indentation
-                        };
-                        JsonDocument jsonDocument = JsonDocument.Parse(response_body);
-
-                        // Serialize the parsed JSON document back to a formatted string
-                        string formattedJson = JsonSerializer.Serialize(jsonDocument.RootElement, options);
-
-                        // Print the formatted JSON
-                        Console.WriteLine(formattedJson);
+                            var weather_object = weather[i];
+                            Console.WriteLine($"{weather_object["date"]} will have a max temp of {weather_object["day"]["maxtemp_f"]} degrees fahrenhiet \nwith a min temp of {weather_object["day"]["mintemp_f"]} degrees fahrenhiet. \nThere will also be a {weather_object["day"]["daily_chance_of_rain"]}% chance of rain\n");
+                        }
                     }
                     else
                     {
@@ -52,10 +45,8 @@ namespace WebDesignTeamWeatherApp
 
         static async Task Main(string[] args)
         {
-
             await GetWeatherData();
-
-            Console.ReadKey(); // You can keep this if you want to wait for a key press after displaying the data.
+            Console.ReadKey();
         }
     }
 }
